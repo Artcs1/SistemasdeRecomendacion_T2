43ef184ce9e4f9564002485302190f34bc4719e4
Artcs1 <ju.jeffri.v@gmail.com>
2018-06-18 17:51:26 -0300

    Update FBC_KNN_metadados.r

--- FBC_KNN_metadados/FBC_KNN_metadados.r
+++ FBC_KNN_metadados/FBC_KNN_metadados.r
@@ -6,6 +6,19 @@ computeTFIDF	<-	function(row)# TF - IDF
     return(w)	
 }
 
+Repart <- function(dataset)
+{
+    dataset = as.matrix(dataset);
+    data = c();
+    for( i in 1:3564) # Iterando sobre todos os items
+    {
+        ids = which(as.numeric(reviewset[,1]) == i) # Numero de reviews da pelicula
+        if(length(ids)!=0)
+            data = rbind(data, dataset[sample(ids,1),]) # Sample de un dato
+    }
+    return (data)
+}
+
 FBC.metadados.model <- function(dataset,testset,reviewset)
 {
     dataset = as.matrix(dataset)
@@ -13,16 +26,16 @@ FBC.metadados.model <- function(dataset,testset,reviewset)
     reviewset = as.matrix(reviewset)
     testset = testset[,2:3]
     
-    users = dataset[,1] #ID dos usuários
+    users = dataset[,1] #ID dos usuÃ¡rios
     movies = dataset[,2] #ID dos filmes
     ratings = dataset[,3] # Ratings
     
-    nusers = max(c(dataset[,1],testset[,1])) # número de usuarios
-    nmovies = max(c(dataset[,2],testset[,2])) # número de filmes
-    scores = matrix(rep(0,nusers*nmovies),nusers,nmovies) # interacción usuario filme 
+    nusers = max(c(dataset[,1],testset[,1])) # nÃºmero de usuarios
+    nmovies = max(c(dataset[,2],testset[,2])) # nÃºmero de filmes
+    scores = matrix(rep(0,nusers*nmovies),nusers,nmovies) # interacciÃ³n usuario filme 
     
     for(i in 1:length(users))
-        scores[users[i],movies[i]] = ratings[i] #construçao da matriz de interação usuario filme
+        scores[users[i],movies[i]] = ratings[i] #construÃ§ao da matriz de interaÃ§Ã£o usuario filme
     
     library(tm)	
     library(SnowballC)
@@ -35,21 +48,20 @@ FBC.metadados.model <- function(dataset,testset,reviewset)
     
     reviewList = as.list(reviews) # Transformando o vetor para uma lista
     nDocs = length(reviewList)
-    print(nDocs)
     reviewList = VectorSource(reviewList)
     corpus = Corpus(reviewList)
-    corpus = tm_map(corpus,	removePunctuation)	# Tokenização
-    corpus = tm_map(corpus, content_transformer(tolower))# Normalização de termos
-    corpus = tm_map(corpus,	stripWhitespace) # Normalização de termos	
+    corpus = tm_map(corpus,	removePunctuation)	# TokenizaÃ§Ã£o
+    corpus = tm_map(corpus, content_transformer(tolower))# NormalizaÃ§Ã£o de termos
+    corpus = tm_map(corpus,	stripWhitespace) # NormalizaÃ§Ã£o de termos	
     
     tdm = 	TermDocumentMatrix(corpus,	control=list(stopwords=TRUE,stemWords=TRUE,wordLengths=c(1,15)))
-    #Remoção de stopwords
-    #Radicalização
+    #RemoÃ§Ã£o de stopwords
+    #RadicalizaÃ§Ã£o
     
     
     m = as.matrix(tdm) # matrix do vocabulario vs termos
     n = t(apply(m,	1,	FUN=computeTFIDF)) # fazer TF - IDF 
-    n = scale(n,	center=FALSE,	scale=sqrt(colSums(n^2))) # normalização
+    n = scale(n,	center=FALSE,	scale=sqrt(colSums(n^2))) # normalizaÃ§Ã£o
     sim = t(n) %*% n # Calculo das similaridades
     
     model = list(score = scores,sim = sim)
@@ -63,18 +75,18 @@ FBC.metadados.predict <- function(model, user , movie, K)
     
     similar.movies	=	order(-simil[movie,])# Ordenando as similiaridades de um filme en forma decrescente
     rated.movies	=	which(score[user,]	>	0)	# Escolher os items que o usuario avalio
-    most.similar.rated	=	intersect(similar.movies,	rated.movies)[1:min(K,length(rated.movies))] #interseção	
+    most.similar.rated	=	intersect(similar.movies,	rated.movies)[1:min(K,length(rated.movies))] #interseÃ§Ã£o	
     
     
-    #Calculo de la predição
+    #Calculo de la prediÃ§Ã£o
     sumSim	=	0	
     sumWeight	=	0
-    if(is.na(most.similar.rated[1]))#Se la interseção e vacia retorna a media
+    if(is.na(most.similar.rated[1]))#Se la interseÃ§Ã£o e vacia retorna a media
     {
         return (3.603814)
     }
     
-    #Calculo de predição con os k vizinhos mais proximos
+    #Calculo de prediÃ§Ã£o con os k vizinhos mais proximos
     for(j	in	most.similar.rated)	{
         sumSim	=	sumSim	+	simil[movie,	j]	
         sumWeight	=	sumWeight	+	simil[movie,	j]	*	score[user,	j]	
@@ -98,14 +110,14 @@ FBC.metadados.test <- function(model,testset,vizinhos,name)
     
     for ( i in 1:tam)
     {
-        ratings[i] = FBC.metadados.predict(model = model, user = testUser[i],movie = testMovie[i],K = vizinhos)  #predição
+        ratings[i] = FBC.metadados.predict(model = model, user = testUser[i],movie = testMovie[i],K = vizinhos)  #prediÃ§Ã£o
     }
     
-    my.dataset <- data.frame(id = ids, rating = ratings) #Criação de um dataframe
+    my.dataset <- data.frame(id = ids, rating = ratings) #CriaÃ§Ã£o de um dataframe
     write.csv(my.dataset,name,row.names=F)  #Exportacion
 }
 
-FBC.metadados.pretest <- function(model,testset) #Devolvera uma lista con los errores cuadraticos de las prediçoes com 1..K viznhos
+FBC.metadados.pretest <- function(model,testset) #Devolvera uma lista con los errores cuadraticos de las prediÃ§oes com 1..K viznhos
 {
     testset = as.matrix(testset[,2:4])
     testUser = testset[,1] #Usuarios
@@ -121,7 +133,7 @@ FBC.metadados.pretest <- function(model,testset) #Devolvera uma lista con los er
     {
         for(j in 1:tam)
         {
-            ratings[j] = FBC.metadados.predict(model = model, user = testUser[j],movie = testMovie[j],K = i)  #predição
+            ratings[j] = FBC.metadados.predict(model = model, user = testUser[j],movie = testMovie[j],K = i)  #prediÃ§Ã£o
         }
         r = ratings;
         print(paste("FBC_KNN_",i,sep=""))
@@ -130,5 +142,5 @@ FBC.metadados.pretest <- function(model,testset) #Devolvera uma lista con los er
         error = c(error,e) # lista de errors
     }
     
-    return (error/sum(error)) # normalização
+    return (error/sum(error)) # normalizaÃ§Ã£o
 }
